62de5287376305de7a5e3774d607e94adbc8b638
Dillon Lee <dillonlee@live.co.uk>
2014-05-22 14:15:07 +0100

    building rows object

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -1,26 +1,80 @@
 app.controller('Risk', function($scope){
-	$scope.name = "Dillon";
 
 	$scope.default = true;
 	$scope.female = false;
 	$scope.alf = false;
 	$scope.age = 60;
 
-
-	$scope.hello = function(){
-	
-			console.log('hello');
-		
+	$scope.rows = {
+		noTherapy : {
+			val : 50,
+			change_val : {
+				female : 10,
+				alf : 20
+			}
+		},
+		aspirin : {
+			val : 75
+		},
+		vka : {
+			val : 17
+		},
+		noak : {
+			val : 12
+		},
+		round : function(treatment){
+			console.log((Math.round(this[treatment].val/10)));
+		}
 	};
-	
+
+	$scope.add = function(){
+
+		if(!$scope.female){
+			$scope.rows.noTherapy.val = $scope.rows.noTherapy.val+10;
+			$scope.rows.aspirin.val = $scope.rows.aspirin.val+5;
+			$scope.rows.vka.val = $scope.rows.vka.val+2;
+			$scope.rows.noak.val = $scope.rows.noak.val+40;
+		}else{
+			$scope.rows.noTherapy.val = $scope.rows.noTherapy.val-10;
+			$scope.rows.aspirin.val = $scope.rows.aspirin.val-5;
+			$scope.rows.vka.val = $scope.rows.vka.val-2;
+			$scope.rows.noak.val = $scope.rows.noak.val-40;
+		}
+
+	for(var key in $scope.rows){
+
+		var obj = $scope.rows[key];
+
+
+		for (var prop in obj) {
+
+			// important check that this is objects own property 
+			// not from prototype prop inherited
+
+			if(obj.hasOwnProperty(prop)){
+
+				console.log(obj.val);
+			}
+		}
+
+			
+	}
+		
+};
+
+
 });
 
 
 app.controller('Row_Graph',function($scope){
 
-	$scope.noTherapy = 50;
-	$scope.aspirin = 75;
-	$scope.vka = 1.7;
-	$scope.noak = 1.2;	
+	// var num = $scope.$parent.rows.noTherapy.round();
+
+	// $('.no_therapy_row .man_icon').removeClass('active');
+
+	// setTimeout(function(){
+	// 	$('.no_therapy_row .man_icon').slice(0,num).addClass('active');
+	// },200)
+
 
 });
